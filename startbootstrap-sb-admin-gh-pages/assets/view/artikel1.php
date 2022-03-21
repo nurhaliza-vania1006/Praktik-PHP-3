ac861b0a4be127f6f50dd8418ca261c0e2aca7a8
Nurhaliza Vania A <93029182+nurhaliza-vania1006@users.noreply.github.com>
2022-03-22 00:00:00 +0700

    Praktik Pemrograman Web PHP 3

---
+++ startbootstrap-sb-admin-gh-pages/assets/view/artikel1.php
@@ -0,0 +1,76 @@
+<?php
+include '../layout/header.php'
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>
+				Apa itu Pemrograman Web ?
+			</h2>
+			<p>
+            <strong>Pemrograman web terbentuk atas 2 kata yaitu Pemrograman dan Web.</strong> 
+            <br>Pemrograman adalah Proses atau Cara dalam menjalankan sebuah urutan intruksi kepada komputer untuk membuat fungsi atau tugas tertentu.
+			dan Web adalah Sistem untuk mengakses, memanipulasi, dan mengunduh dokumen yang terdapat pada komputer yang di hubungkan melalui internet atau jaringan.
+            <strong>Jadi Pemrograman Web adalah Proses atau Cara untuk menjalankan intruksi pada sebuah komputer yang terhubung ke internet untuk membuat fungsi atau tugas tertentu.</strong>
+            </p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<h3>
+				Jenis - Jenis Website
+			</h3>
+			<ol>
+				<li class="list-item">
+				    <strong> Web Statis</strong>
+                    <p>
+                    Website Statis merupakan website yang isi web tidak diperbaharui dalam kurun waktu tertentu, 
+                    sehingga isi konten tersebut dari waktu ke waktu akan selalu tetap sama seperti pada saat awal dibuat.
+                    </p>
+				</li>
+				<li class="list-item">
+                    <strong> Web Dinamis</strong>
+                    <p>
+                    Website Dinamis merupakan website yang selalu diperbaharui kontennya oleh pemilik atau pengelola website. 
+                    Contoh dari website ini adalah Blog dan website berita online.
+                    </p>
+				</li>
+				<li class="list-item">
+					<strong>Web Interaktif</strong>
+                    <p>
+                    Website ini hampir mirip dengan website dinamis. Namun yang menjadi pembeda utamanya adalah konten webnya 
+                    tidak hanya di perbaharui oleh pengelola website tetapi lebih banyak dilakukan oleh pengguna/user website tersebut.
+                    </p>
+				</li>
+			</ol>
+		</div>
+		<div class="col-md-6">
+			<h3>
+				Macam-Macam Bahasa Pemrograman 
+			</h3>
+			<ul>
+				<li class="list-item">
+                    HTML (HyperText Markup Language)
+				</li>
+				<li class="list-item">
+                    CSS (Cascading​​ Style Sheet)
+				</li>
+				<li class="list-item">
+					JavaScript
+				</li>
+				<li class="list-item">
+                    PHP (PHP Hypertext Pre-Processor)
+				</li>
+			</ul>
+		</div>
+	</div>
+    <P> Sumber : 
+        <br>https://badoystudio.com/belajar-pemrograman-web/
+        <br>https://www.teknik.unpas.ac.id/blogs/pemrograman-web/
+    </P>
+</div>
+
+<?php
+include '../layout/footer.php'
+?>
---
+++ startbootstrap-sb-admin-gh-pages/assets/view/artikel2.php
@@ -0,0 +1,119 @@
+<?php
+include '../layout/header.php'
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				Tips Memulai Bisnis Start Up
+			</h3>
+			<p>
+            Ketika merintis sebuah bisnis, penting bagi kalian untuk memliki strategi yang matang. Strategi yang matang akan membantu 
+            untuk memecahkan masalah dan memudahkan dalam proses memulai bisnis startup.
+			</p>
+		</div>
+	</div>
+    <div class="row">
+		<div class="col-md-12">
+			<h3>
+				Apa itu Bisnis Start Up ?
+			</h3>
+			<p>
+            <strong>Bisnis Startup</strong> adalah bisnis rintisan yang diharap dapat tumbuh dan menguasai ceruk pasar secara cepat dan selanjutnya menjadi perusahaan yang besar.
+            Bisnis startup biasanya lebih mengutamakan ide-ide baru dan memberi solusi permasalahan bagi konsumen.
+            Bisnis startup juga biasanya mengandalkan teknologi untuk membantu pertumbuhan bisnis, maka tidak salah jika sebagian orang memandang bisnis startup 
+            merupakan bisnis yang bergerak di bidang teknologi.
+			</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<h3>
+				Karakteristik Bisnis Startup
+			</h3>
+			<ul>
+				<li class="list-item">
+                    Modal yang digunakan akan tergantung dengan pergerakan bisnis
+				</li>
+				<li class="list-item">
+                    Usia perusahaan kurang dari 3 tahun
+				</li>
+                <li class="list-item">
+                    Pendapatan perusahaan selama satu tahun kurang dari USD 100.000
+				</li>
+				<li class="list-item">
+                    Berfokus pada perkembangan bisnis yang cepat
+				</li>
+				<li class="list-item">
+                    Mengandalkan lebih banyak teknologi  informasi dan elektronik
+				</li>
+			</ul>
+		</div>
+		<div class="col-md-6">
+			<h3>
+				Persiapan Dalam Membangun Bisnis Startup
+			</h3>
+			<ul>
+				<li class="list-item">
+                Temukan ide bisnis startup yang akan dibuat
+				</li>
+				<li class="list-item">
+                Pilih nama domain yang tepat
+				</li>
+				<li class="list-item">
+                Cari target pasar 
+				</li>
+				<li class="list-item">
+                Membuat penawaran atau produk yang spesifik
+				</li>
+				<li class="list-item">
+                Focus terhadap tujuan awal
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				Faktor Perkembangan Bisnis Startup di Indonesia
+			</h3>
+			<ol type="a">
+				<li class="list-item">
+                   <strong>Masyarakat Indonesia selalu terbuka dengan teknologi baru.</strong> 
+                   <p>
+                   Dapat dilihat saat ini ada banyak sekali pengguna Internet aktif di Indonesia maka akan 
+                    pada peluang bisnis yang juga semakin besar.
+                   </p>
+				</li>
+				<li class="list-item">
+                <strong>Jumlah penduduk Indonesia yang banyak.</strong> 
+                   <p>
+                   Banyaknya penduduk maka kebutuhannya juga semakin tinggi dan tentunya juga akan menjadi peluang bisnis yang besar.
+                   </p>
+				</li>
+				<li class="list-item">
+                <strong>Pelayanan Startup yang baik.</strong> 
+                   <p>
+                   Semakin baik dan bagus pelayanan sebuah startup yang diberikan, maka hal ini merupakan karaktersistik bisnis sehingga konsumen juga semakin banyak.
+                   </p>
+				</li>
+				<li class="list-item">
+                <strong>Modal dari investor serta dukungan pemerintah..</strong> 
+                   <p>
+                   Tanpa adanya modal, maka bisnis apapun tidak akan bisa berkembang dengan baik. Maka dukungan dari pemerintah juga menjadi penentu berkembangnya bisnis startup, 
+                   jika pemerintah tidak mendukung maka startup sangat sulit untuk berkembang.
+                   </p>
+				</li>
+			</ol>
+		</div>
+	</div>
+    <P> Sumber : 
+        <br>https://www.niagahoster.co.id/blog/cara-memulai-bisnis-startup/
+        <br>https://www.niagahoster.co.id/blog/cara-membangun-startup/
+        <br>https://www.jurnal.id/id/blog/mengenal-bisnis-startup-peluang-dan-tips-untuk-memulainya/
+</div>
+
+<?php
+include '../layout/footer.php'
+?>
